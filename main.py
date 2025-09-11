4c662ad7baee4f581cd87007947de404460f4305
Turjoy-barua <84409077+Turjoy-barua@users.noreply.github.com>
2025-09-11 23:28:07 +0200

    api added

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = 'c347ad0525998efbcb7008dd10e7d719'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}") # todays weather
+historical_data = requests.get()
+
+'''if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")'''
+print(weather_data.json())
